c90401192326ad20f9a0ea1b89eff4ab77917977
TrueFalafel <jonuma18@gmail.com>
2017-04-01 18:34:27 +0100

    Recebo dados de tempo e humidade (TESTADO)

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,3 +1,6 @@
+#include <SparkFunTSL2561.h>
+#include <ESP8266.h>
+#include <doxygen.h>
 #include <dht11.h>
 #define HUM_PIN A0
 #define TEMP_PIN 3 //digital input
@@ -5,11 +8,50 @@
   # 0  ~300     dry soil
   # 300~700     humid soil
   # 700~950     in water        */
+  
+/* WiFi macros*/
+#define SSID        "NOME_DA_SUA_REDE_WIFI"
+#define PASSWORD    "senha_da_sua_rede"
+//inputs
+#define CH_PD 4
+#define RST 5
+#define GPIO0 6
+
+ESP8266 wifi(Serial);
 
 void setup() {
-  // Comunicates with pc at 57600 bit/s
-  Serial.begin(57600);
+  // Comunicates with pc and WIFi module at 9600 bit/s
+  Serial.begin(9600);
+  //waits one second after power up
+  while(millis() < 1000);
 
+  pinMode(TEMP_PIN, INPUT);
+  /*
+  pinMode(CH_PD,OUTPUT);
+  pinMode(RST,OUTPUT);
+  pinMode(GPIO0,OUTPUT);
+  digitalWrite(CH_PD,HIGH);
+  digitalWrite(RST,HIGH); 
+  digitalWrite(GPIO0,HIGH);
+
+  if(wifi.kick())
+    Serial.println("wifi module OK!");
+  else
+    Serial.println("wifi module NOT OK!");
+
+  if(wifi.setOprToStation())
+    Serial.println("Station OK.");
+  else
+    Serial.println("Station ERROR");
+
+  if (wifi.joinAP(SSID, PASSWORD)){
+      Serial.println("Successful connection");
+      Serial.print("IP: ");
+      Serial.println(wifi.getLocalIP().c_str());    
+  }else{
+      Serial.println("Falha na conexao AP.");
+  }
+  */
 }
 
 void loop() {
@@ -40,6 +82,11 @@ void loop() {
       Serial.println("Unknown error"); 
       break;
   }
+
+  Serial.print("Temperature (°C): ");
+  Serial.println((float)DHT11.temperature, 1);
+  Serial.print("Humidity: ");
+  Serial.println(hum_read);
 
   delay(2000);
   
