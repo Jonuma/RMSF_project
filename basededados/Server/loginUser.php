c316d04b7b6bf67d547f384aa1725b705772d628
Joana Baleiras <Joana Baleiras>
2017-05-22 19:51:31 +0100

    server,signIn,login e a base de dados (measurements)

---
+++ basededados/Server/loginUser.php
@@ -0,0 +1,69 @@
+<?php session_start(); ?>
+<html>
+	<head><title>Login from the app</title></head>
+	<body>
+		<h1>LOGIN</h1>
+		<?php
+		
+			$host = "db.ist.utl.pt";
+			$user = "ist178094";
+			$password = "kier6808";
+			$dsn = "mysql:host=$host;dbname=$user";
+		
+			try{
+				$connection = new PDO($dsn, $user, $password);
+			}
+			catch(PDOException $exception){
+				echo("<p>Error: ");
+				echo($exception->getMessage());
+				echo("</p>");
+				exit();
+			}
+				
+			// array for JSON response
+			$response = array();
+			
+			$email = htmlentities($_GET['email'], ENT_QUOTES);
+			$pass = htmlentities($_GET['pass'], ENT_QUOTES);
+			
+			$result = $connection->prepare("Select username from users where username=:email");
+			$result->bindParam(':email', $email);
+			$result->execute();
+			
+			if( ($result->rowcount())!=0 ){
+				$response[0]=1;
+				//$response["error"] = false;
+				//$response["message"] = "Logged in successfully!";
+			}else{
+				$response[0]=0;
+				//$response["error"] = true;
+				//$response["message"] = "Failed to login!";
+			}
+			
+			
+			
+		//	if(($result->rowcount())==0){			
+			
+		//		$sql = "INSERT INTO users VALUES ('$email', '$pass')";
+		//		$result = $connection->exec($sql);
+			
+				//$result = $connection->prepare("insert into users values(:email, :pass);");
+				//$result->bindParam(':email', $user);
+				//$result->bindParam(':pass', $pass);
+				//$result->execute();
+				
+		//		$response[0]=1;
+				//$response["error"] = false;
+				//$response["message"] = "Signed in successfully!";
+		//	}else{
+		//		$response[0]=0;
+				//$response["error"] = true;
+				//$response["message"] = "Failed to sign in!";
+		//	}
+			echo json_encode($response);
+
+			$connection=null;
+	
+		?>
+	</body>
+</html>
--- basededados/measurements.sql
+++ basededados/measurements.sql
@@ -1,14 +1,23 @@
 drop table if exists measurements;
+drop table if exists users;
 
 create table measurements(
-	minutes datetime, 
+	minutes timestamp, 
 	temperature integer,
 	light integer,  
 	moisture integer,
-	primary key(minutes));   	
+	primary key(minutes)); 
+
+create table users(
+	username varchar(255),
+	password varchar(255),
+	primary key(username));
 	
 	
 insert into measurements values('2017-03-18 21:25:00', 23, 110, 370);
 insert into measurements values('2017-03-18 18:50:00', 30, 100, 372);
 insert into measurements values('2017-03-19 08:35:00', 19, 80, 370);
-	
+
+insert into users values('jmbaleiras', '0op01');
+insert into users values('cats', '0op01jk');
+insert into users values('joaom', '01glo');
