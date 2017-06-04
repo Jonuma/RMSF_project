3382941762247ff7b0d118c548787314d84d7a35
Joana Baleiras <Joana Baleiras>
2017-06-04 21:39:24 +0100

    measurements with trigger

--- basededados/measurements.sql
+++ basededados/measurements.sql
@@ -1,38 +1,42 @@
+/*drop trigger if exists check_datetime;*/
 drop table if exists measurements;
 drop table if exists users;
 
-create table measurements(
-	minutes timestamp, 
-	temperature integer,
-	light integer,  
-	moisture integer,
-	idArduino varchar(255),
-	primary key(minutes)); 
 
 create table users(
+	idArduino varchar(255),
 	email varchar(255),
 	password varchar(255),
 	configInt integer,
-	primary key(email));
+	primary key(idArduino));
+
+create table measurements(
+	idArduino varchar(255),
+	minutes timestamp,
+	temperature integer,
+	light integer,  
+	moisture integer,
+	primary key(minutes),
+	foreign key(idArduino) references users(idArduino) ); 
 	
-insert into measurements values('2017-03-18 21:25:00', 23, 110, 370, 'ola');
+/*
+insert into measurements values('2017-06-03%21:25:00', 23, 110, 370, 'ola');
 insert into measurements values('2017-03-18 18:50:00', 30, 100, 372, 'olaaa');
+*/
 
-insert into users values('jmbaleiras', '0op01', '2');
-insert into users values('cats', '0op01jk', '2');
-insert into users values('joaom', '01glo', '2');
+insert into users values('a0:20:a6:15:42:2e', 'ju@cats.jo', 'olaole', '2');
 
+/*
+delimiter $$
 create trigger check_datetime before insert on measurements
 for each row
 begin
-	if exists (select * from measurements as m where day(new.minutes) = day(m.minutes)+1 and hour(new.minutes) = hour(m.minutes)) then
+	if exists (select * from measurements as m where DAY(m.minutes)+1 = DAY(new.minutes) and hour(m.minutes) = hour(new.minutes)) then
 	
-		delete from measurements as m
-		where day(new.minutes) = day(m.minutes)+1 and hour(new.minutes) = hour(m.minutes)
-		limit 1; --vi que "limit [int]" significa o numero que esta em "int" e o numero de linhas que ele apaga devido ao "limit"
-		
+		delete from measurements where hour(minutes) = hour(new.minutes);
 		insert into measurements values(new.idArduino, new.minutes, new.temperature, new.light, new.moisture);
 	
 	end if;
-
-end 
+end$$
+delimiter ;
+*/
